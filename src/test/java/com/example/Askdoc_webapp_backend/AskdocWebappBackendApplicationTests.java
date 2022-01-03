26f4f5b9d22078f6108cc5ede1f254a7c0aaf25f
shubhampatil7775 <shubhampatil.sp7775@gmail.com>
2022-01-03 12:34:23 +0530

    Initial commit

---
+++ src/test/java/com/example/Askdoc_webapp_backend/AskdocWebappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Askdoc_webapp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AskdocWebappBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
