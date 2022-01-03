43273952e5fef9ddf6a10536a797d7981bae99dc
shubhampatil7775 <shubhampatil.sp7775@gmail.com>
2022-01-03 12:34:23 +0530

    Added user entity and apis

---
+++ src/main/java/com/example/Askdoc_webapp_backend/User/UserController.java
@@ -0,0 +1,28 @@
+package com.example.Askdoc_webapp_backend.User;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        return userService.getUserById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+}
+
--- src/main/resources/db/migration/V1__Create_Auth_table.sql
+++ src/main/resources/db/migration/V1__Create_Auth_table.sql
@@ -1,4 +1,4 @@
-CREATE TABLE users (
+CREATE TABLE Auth (
                        id SERIAL PRIMARY KEY,
                        username VARCHAR(50) NOT NULL,
                        password VARCHAR(100) NOT NULL,
